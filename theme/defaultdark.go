76562a05acaad4e1b3ea7ad7dbabcfcaacddfac4
Halsten <leeser.lionel@gmail.com>
2025-08-23 15:37:25 +0200

    no more circular import

---
+++ theme/defaultdark.go
@@ -0,0 +1,51 @@
+package theme
+
+import (
+	"github.com/charmbracelet/lipgloss"
+)
+
+type DefaultDarkTheme struct{}
+
+func (d *DefaultDarkTheme) Style(style StyleName) lipgloss.Style {
+	var s lipgloss.Style
+
+	s = lipgloss.NewStyle()
+
+	switch style {
+	case TitleStyleName:
+		s = s.Bold(true).Foreground(d.Color(TitleFontColorName))
+
+	case NormalTextName:
+		s = s.Foreground(d.Color(NormalFontColorName))
+
+	case FocusedWidgetStyleName:
+		s = s.Border(lipgloss.RoundedBorder()).
+			BorderForeground(d.Color(FocusedBorderColorName))
+
+	case BlurredWidgetStyleName:
+		s = s.Border(lipgloss.RoundedBorder()).
+			BorderForeground(d.Color(BlurredBorderColorName))
+
+	}
+
+	return s
+}
+
+func (d *DefaultDarkTheme) Color(color ColorName) lipgloss.Color {
+	var colorHexCode string
+
+	switch color {
+	case BlurredBorderColorName, BlurredFontColorName:
+		colorHexCode = "#186318"
+
+	default:
+		colorHexCode = "#18B718"
+
+	}
+
+	return lipgloss.Color(colorHexCode)
+}
+
+func (d *DefaultDarkTheme) Size(size SizeName) int {
+	return 0
+}
