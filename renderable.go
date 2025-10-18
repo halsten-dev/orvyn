defd8bfb34e5110f08da1de4a5c80acedcb14552
Halsten <leeser.lionel@gmail.com>
2025-10-18 16:55:26 +0200

    Added more control over min and preferred size

--- renderable.go
+++ renderable.go
@@ -10,20 +10,27 @@ type Renderable interface {
 	Render() string
 	Resize(Size)
 	GetSize() Size
+	SetMinSize(Size)
 	GetMinSize() Size
+	SetPreferredSize(Size)
 	GetPreferredSize() Size
 }
 
 type BaseRenderable struct {
 	BaseActivable
 
-	size Size
+	size          Size
+	minSize       Size
+	preferredSize Size
 }
 
 func NewBaseRenderable() BaseRenderable {
 	b := BaseRenderable{}
 
 	b.BaseActivable = NewBaseActivable()
+
+	b.minSize = NewSize(1, 1)
+	b.preferredSize = NewSize(1, 1)
 
 	return b
 }
@@ -36,12 +43,20 @@ func (b *BaseRenderable) GetSize() Size {
 	return b.size
 }
 
+func (b *BaseRenderable) SetMinSize(size Size) {
+	b.minSize = size
+}
+
 func (b *BaseRenderable) GetMinSize() Size {
-	return NewSize(1, 1)
+	return b.minSize
+}
+
+func (b *BaseRenderable) SetPreferredSize(size Size) {
+	b.preferredSize = size
 }
 
 func (b *BaseRenderable) GetPreferredSize() Size {
-	return NewSize(1, 1)
+	return b.preferredSize
 }
 
 type Updatable interface {
