6792b692c443b20b7833bc6892e0fddb032f4202
Halsten <leeser.lionel@gmail.com>
2025-10-25 17:51:18 +0200

    Tabindex management when inserting new widget in the focus manager

--- focusmanager.go
+++ focusmanager.go
@@ -58,6 +58,10 @@ func (f *FocusManager) Insert(index int, widget Focusable) {
 	}
 
 	f.widgets = append(f.widgets[:index+1], f.widgets[index:]...)
+
+	if index <= f.tabIndex {
+		f.getNextIndex()
+	}
 }
 
 // Update allows to change the widget at the given index.
